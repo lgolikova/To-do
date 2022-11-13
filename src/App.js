e77acfede89677f7635276e9136ea03f4564e917
lgolikova <milagoul@gmail.com>
2022-11-13 14:33:48 +0300

    added some styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Footer from './components/footer';
 import Header from './components/header';
 import Main from './components/main';
+import ToDoItem from './components/todoitem';
 
 
 
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
-      <Footer />
+      {/* <Header /> */}
+      <div className='todo-list'>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,19 +3,7 @@ import React from 'react';
 function Main() {
     return (
         <main>
-            <div>
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
 
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-            </div>
         </main>
     );
 }
