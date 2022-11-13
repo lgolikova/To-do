c0e155dd903230b5713aaf549d691fc67dc2db96
lgolikova <milagoul@gmail.com>
2022-11-13 14:04:39 +0300

    starting the project

---
+++ src/components/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main>
+            <div>
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
